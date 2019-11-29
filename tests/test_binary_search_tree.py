1416f34bb919101b5f479ca1285e149f364b939c
Mason Moreland <mazc@protonmail.ch>
2019-11-28 22:14:26 -0800

    Incomplete BST implementation and repo restructuring

---
+++ tests/test_binary_search_tree.py
@@ -0,0 +1,10 @@
+import unittest
+from binary_search_tree import *
+
+class BinarySearchTreeTest(unittest.TestCase):
+    def test_insert(self):
+        self.assertTrue(True)
+        
+
+if __name__=="__main__":
+    unittest.main()
