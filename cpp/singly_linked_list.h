521a0cf2d74df9676ba36c7bd76188acdd5f37db
Mason Moreland <mazc@protonmail.ch>
2019-12-04 23:32:17 -0800

    Add catch cpp tests for linked list size

--- cpp/singly_linked_list.h
+++ cpp/singly_linked_list.h
@@ -1,25 +1,31 @@
 #ifndef SINGLY_LINKED_LIST_H_
 #define SINGLY_LINKED_LIST_H_
 
+#include <cstddef>
+
 template <class T>
 struct Node
 {
     T data;
-    Node *next;
+    Node<T> *next;
 };
 
 template <class T>
 class SinglyLinkedList
 {
-    SinglyLinkedList();
+    public:
+        SinglyLinkedList();
 
-    bool empty() const;
-    size_t size() const;
+        bool empty() const;
+        size_t size() const;
 
-    void push_front(const T& val);
-    void pop_front();
-    void push_back(const T& val);
-    void pop_back();
+        void push_front(const T& val);
+        void pop_front();
+        void push_back(const T& val);
+        void pop_back();
+
+    private:
+        Node<T> *head;
 };
 
 #endif /* SINGLY_LINKED_LIST_H_ */
--- cpp/test_singly_linked_list.cpp
+++ cpp/test_singly_linked_list.cpp
@@ -1,3 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
+#include "singly_linked_list.h"
+
+TEST_CASE( "Size of linked list" )
+{
+    SinglyLinkedList<int> linkedList;
+
+    SECTION("Empty linked list")
+        REQUIRE(linkedList.size() == 0);
+
+    SECTION("Non-empty linked list")
+        for (int i = 0; i < 5; i++)
+        {
+            linkedList.push_back(i);
+        }
+        REQUIRE(linkedList.size() == 5);
+}
