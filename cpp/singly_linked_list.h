1c16b0345e7f3b3d4555ddefe580ddaf017f100a
Mason Moreland <mazc@protonmail.ch>
2019-12-05 23:56:46 -0800

    Add singly linked list cpp unit tests

--- cpp/singly_linked_list.h
+++ cpp/singly_linked_list.h
@@ -2,6 +2,7 @@
 #define SINGLY_LINKED_LIST_H_
 
 #include <cstddef>
+#include <iostream>
 
 template <class T>
 struct Node
@@ -14,18 +15,125 @@ template <class T>
 class SinglyLinkedList
 {
     public:
-        SinglyLinkedList();
+        SinglyLinkedList<T>();
 
         bool empty() const;
         size_t size() const;
 
         void push_front(const T& val);
-        void pop_front();
         void push_back(const T& val);
+        void pop_front();
         void pop_back();
 
     private:
         Node<T> *head;
 };
+
+template <class T>
+SinglyLinkedList<T>::SinglyLinkedList()
+{
+    this->head = nullptr;
+}
+
+template <class T>
+bool SinglyLinkedList<T>::empty() const
+{
+    return head ? false : true;
+}
+
+template <class T>
+size_t SinglyLinkedList<T>::size() const
+{
+    size_t count = 0;
+    Node<T> *ptr = head;
+    while (ptr != nullptr)
+    {
+        ptr = ptr->next;
+        count++;
+    }
+
+    return count;
+}
+
+template <class T>
+void SinglyLinkedList<T>::push_front(const T& val)
+{
+    Node<T> *node = new Node<T>();
+    node->data = val;
+
+    if (head == nullptr)
+    {
+        head = node;
+    }
+    else
+    {
+        node->next = head;
+        head = node;
+    }
+    
+}
+
+template <class T>
+void SinglyLinkedList<T>::push_back(const T& val)
+{
+    Node<T> *node = new Node<T>();
+    node->data = val;
+
+    if (head == nullptr)
+    {
+        head = node;
+    }
+    else
+    {
+        Node<T> *ptr = head;
+        while (ptr->next != nullptr)
+        {
+            ptr = ptr->next;
+        }
+
+        ptr->next = node;
+    }
+}
+
+template <class T>
+void SinglyLinkedList<T>::pop_front()
+{
+    if (head)
+    {
+        if (head->next)
+        {
+            head = head->next;
+        }
+        else
+        {
+            head = nullptr;
+        }
+    }
+}
+
+template <class T>
+void SinglyLinkedList<T>::pop_back()
+{
+    if (head)
+    {
+        Node<T> *ptr = head;
+
+        if (ptr->next == nullptr)
+        {
+            delete ptr;
+            ptr = nullptr;
+        }
+        else
+        {
+            while (ptr->next != nullptr)
+            {
+                std::cout << ptr->data << "\n";
+                ptr = ptr->next;
+            }
+            delete ptr;
+            ptr = nullptr;
+        }
+    }
+}
 
 #endif /* SINGLY_LINKED_LIST_H_ */
--- cpp/test_singly_linked_list.cpp
+++ cpp/test_singly_linked_list.cpp
@@ -3,17 +3,116 @@
 #include "catch.hpp"
 #include "singly_linked_list.h"
 
+TEST_CASE("Linked list push front")
+{
+    SinglyLinkedList<int> linkedList;
+
+    SECTION("Push to empty linked list")
+    {
+        linkedList.push_front(0);
+        REQUIRE(linkedList.size() == 1);
+    }
+}
+
+TEST_CASE("Linked list push back")
+{
+    SinglyLinkedList<int> linkedList;
+
+    SECTION("Push to empty linked list")
+    {
+        linkedList.push_back(0);
+        REQUIRE(linkedList.size() == 1);
+    }
+}
+
+TEST_CASE("Linked list pop front")
+{
+    SinglyLinkedList<int> linkedList;
+
+    SECTION("Pop from empty linked list")
+    {
+        linkedList.pop_front();
+        REQUIRE(linkedList.empty());
+    }
+
+    SECTION("Pop from list of size 1")
+    {
+
+        linkedList.push_back(0);
+        linkedList.pop_front();
+        REQUIRE(linkedList.empty());
+    }
+
+    SECTION("Pop from list of size greater than 1")
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            linkedList.push_back(i);
+        }
+        linkedList.pop_front();
+        REQUIRE(linkedList.size() == 4);
+    }
+}
+
+TEST_CASE("Linked list pop back")
+{
+    SinglyLinkedList<int> linkedList;
+
+    SECTION("Pop from empty linked list")
+    {
+        linkedList.pop_back();
+        REQUIRE(linkedList.empty());
+    }
+
+    SECTION("Pop from list of size 1")
+    {
+        linkedList.push_back(0);
+        linkedList.pop_back();
+        REQUIRE(linkedList.empty());
+    }
+
+    SECTION("Pop from list of size greater than 1")
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            linkedList.push_back(i);
+        }
+        linkedList.pop_back();
+        REQUIRE(linkedList.size() == 4);
+    }
+}
+
+TEST_CASE("Linked list empty")
+{
+    SinglyLinkedList<int> linkedList;
+    
+    SECTION("Empty linked list")
+    {
+        REQUIRE(linkedList.empty());
+    }
+
+    SECTION("Non-empty linked list")
+    {
+        linkedList.push_back(0);
+        REQUIRE(!linkedList.empty());
+    }
+}
+
 TEST_CASE( "Size of linked list" )
 {
     SinglyLinkedList<int> linkedList;
 
     SECTION("Empty linked list")
-        REQUIRE(linkedList.size() == 0);
+    {
+        REQUIRE(linkedList.empty());
+    }
 
     SECTION("Non-empty linked list")
+    {
         for (int i = 0; i < 5; i++)
         {
             linkedList.push_back(i);
         }
         REQUIRE(linkedList.size() == 5);
+    }
 }
